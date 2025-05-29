d3841a28a23145d6e48efc5eb21217cf31745215
Piootrekk <piotr.wilkowiecki100@gmail.com>
2025-05-29 23:45:12 +0200

    show hide listings items inside steam market

--- steam-chrome-extension/src/popup.ts
+++ steam-chrome-extension/src/popup.ts
@@ -1,3 +1,32 @@
-import browser from "webextension-polyfill";
+const generateButton = (textInside: string) => {
+  const button = document.createElement("button");
+  button.textContent = textInside;
+  button.classList.add("custom-button");
+  button.setAttribute("data-hidden", "false");
+  button.addEventListener("click", () => {
+    const isHidden = button.getAttribute("data-hidden") === "true";
+    button.setAttribute("data-hidden", (!isHidden).toString());
+    button.textContent = isHidden ? "<< hide >>" : "<< unhide >>";
+  });
+  return button;
+};
 
-console.log("Hello from the popup!", { id: browser.runtime.id });
+const buttonAction = () => {};
+
+const injectButton = (): void => {
+  const listingCards = document.querySelectorAll(
+    ".market_content_block.my_listing_section.market_home_listing_table"
+  );
+  if (listingCards.length === 0) return;
+  listingCards.forEach((card) => {
+    const header = card.querySelector(".my_market_header");
+    const listingsItems = card.querySelectorAll(
+      ".market_listing_row.market_recent_listing_row"
+    );
+
+    if (header === null) throw Error("Header not found");
+    header.appendChild(generateButton("<< hide >>"));
+  });
+};
+
+injectButton();

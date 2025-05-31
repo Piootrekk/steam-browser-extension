673ef520507e65f772da500a4799b8dbe35fc6cd
Piootrekk <piotr.wilkowiecki100@gmail.com>
2025-05-31 02:41:15 +0200

    changes in hide & show implementation

---
+++ readme.md
@@ -0,0 +1,3 @@
+### Creating my own steam extension, because steam inventory helper, steam trading enhancer sucks
+
+#### No more ads/external marketplaces, massive api calls
--- steam-chrome-extension/src/popup.ts
+++ steam-chrome-extension/src/popup.ts
@@ -1,48 +1,37 @@
-const hideButton = (list: NodeListOf<Element>) => {
+const generatehidingButton = (element: Element) => {
+  const wrapper = document.createElement("div");
   const button = document.createElement("button");
-  button.textContent = "<< hide >>";
+  button.textContent = "<< HIDE >>";
   button.classList.add("custom-button");
   button.setAttribute("data-hidden", "false");
   button.addEventListener("click", () => {
-    const currentHidden = button.getAttribute("data-hidden") === "true";
-    const isHidden = !currentHidden;
-    button.setAttribute("data-hidden", isHidden.toString());
-    button.textContent = isHidden ? "<< unhide >>" : "<< hide >>";
-    buttonAction(isHidden, list);
+    const isHidden = button.getAttribute("data-hidden") === "true";
+    button.setAttribute("data-hidden", (!isHidden).toString());
+    if (!isHidden) {
+      if (element instanceof HTMLElement === false) return;
+      button.textContent = "<< UNHIDE >>";
+      element.style.display = "none";
+    } else if (isHidden) {
+      if (element instanceof HTMLElement === false) return;
+      button.textContent = "<< HIDE >>";
+      element.style.display = "block";
+    }
   });
-  return button;
+  wrapper.appendChild(button);
+  return wrapper;
 };
 
-const buttonAction = (
-  isHidden: boolean,
-  listingsItems: NodeListOf<Element>
-) => {
-  if (isHidden) {
-    listingsItems.forEach((listing) => {
-      if (listing instanceof HTMLElement === false) return;
-      listing.style.display = "none";
-    });
-  } else {
-    listingsItems.forEach((listing) => {
-      if (listing instanceof HTMLElement === false) return;
-      listing.style.display = "block";
-    });
-  }
-};
-
-const injectButton = (): void => {
-  const listingCards = document.querySelectorAll(
-    ".market_content_block.my_listing_section.market_home_listing_table"
+const injectHideButtons = () => {
+  const listingsMainContainer = document.querySelector<HTMLElement>(
+    "#tabContentsMyListings"
   );
-  if (listingCards.length === 0) return;
-  listingCards.forEach((card) => {
-    const header = card.querySelector(".my_market_header");
-    const listingsItems = card.querySelectorAll(
-      ".market_listing_row.market_recent_listing_row"
-    );
-    if (header === null) return;
-    header.appendChild(hideButton(listingsItems));
+  if (!listingsMainContainer) throw new Error("Main container not exist");
+  listingsMainContainer.style.paddingTop = "30px";
+  const childs = Array.from(listingsMainContainer.children);
+  childs.forEach((child) => {
+    const button = generatehidingButton(child);
+    listingsMainContainer.insertBefore(button, child);
   });
 };
 
-injectButton();
+injectHideButtons();

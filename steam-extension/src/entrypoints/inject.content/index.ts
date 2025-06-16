7e7996276e0ea5ae8165ffc36e5c139643d8ac01
Piootrekk <piotr.wilkowiecki100@gmail.com>
2025-06-16 20:50:52 +0200

    base calculations for all buy orders/ sell orders

--- steam-extension/src/entrypoints/inject.content/index.ts
+++ steam-extension/src/entrypoints/inject.content/index.ts
@@ -2,13 +2,8 @@ export default defineContentScript({
   matches: ["https://steamcommunity.com/market/"],
   runAt: "document_start",
   async main() {
-    // const scriptUrl = browser.runtime.getURL("/intercept.js");
-    // const script = document.createElement("script");
     await injectScript("/intercept.js", {
       keepInDom: true,
     });
-    // script.src = scriptUrl;
-
-    // document.documentElement.appendChild(script);
   },
 });
--- steam-extension/src/entrypoints/market.content/listings.ts
+++ steam-extension/src/entrypoints/market.content/listings.ts
@@ -1,5 +1,7 @@
-const generateHidingButton = (element: Element) => {
-  const wrapper = document.createElement("div");
+import { getSumActiveOrders, getSumBuyOrders } from "./items-calculation";
+
+const generateHidingButton = (element: HTMLElement) => {
+  const divWrapper = document.createElement("div");
   const button = document.createElement("button");
   button.textContent = "<< HIDE >>";
   button.classList.add("hidding-button");
@@ -8,25 +10,44 @@ const generateHidingButton = (element: Element) => {
     const isHidden = button.getAttribute("data-hidden") === "true";
     button.setAttribute("data-hidden", (!isHidden).toString());
     if (!isHidden) {
-      if (element instanceof HTMLElement === false) return;
       button.textContent = "<< SHOW >>";
       element.style.display = "none";
+      divWrapper.style.paddingBottom = "0px";
+      button.style.marginBottom = "0px";
     } else if (isHidden) {
-      if (element instanceof HTMLElement === false) return;
       button.textContent = "<< HIDE >>";
       element.style.display = "block";
+      divWrapper.style.paddingBottom = "0px";
+      button.style.marginBottom = "0px";
     }
   });
-  wrapper.appendChild(button);
-  button.style.paddingBottom = "20px";
+  divWrapper.appendChild(button);
   button.style.fontSize = "17px";
-  return wrapper;
+  button.style.fontWeight = "bold";
+  return divWrapper;
 };
 
-const appendChangesToListings = (listingsMainContainer: HTMLElement | null) => {
-  if (!listingsMainContainer) return;
+const getMyOrdersSum = () => {
+  const note =
+    "Total listed price: NaNzł You will receive: 27,56zł (on this page) My buy orders (767)";
+};
+
+const getBuyOrderSum = () => {
+  const note =
+    "Orders placed total value: NaNzł Max allowed by current balance: 0,00zł You can set more orders totaling: NaNzł";
+};
+
+const spawnSumNotification = () => {
+  const divElement = document.createElement("div");
+  divElement.textContent =
+    "Total listed price: NaNzł You will receive: 27,56zł (on this page) My buy orders (767";
+  return divElement;
+};
+
+const injectButtonsForContent = (listingsMainContainer: HTMLElement) => {
   listingsMainContainer.style.paddingTop = "30px";
-  const activeListings = listingsMainContainer.querySelectorAll(
+  listingsMainContainer.style.paddingBottom = "0px";
+  const activeListings = listingsMainContainer.querySelectorAll<HTMLElement>(
     ".my_listing_section.market_content_block"
   );
   const hiddingButtons =
@@ -35,8 +56,17 @@ const appendChangesToListings = (listingsMainContainer: HTMLElement | null) => {
   const childs = Array.from(activeListings);
   childs.forEach((child) => {
     const button = generateHidingButton(child);
+    const sumInfo = spawnSumNotification();
     listingsMainContainer.insertBefore(button, child);
+    listingsMainContainer.insertBefore(sumInfo, child);
   });
+};
+
+const appendChangesToListings = (listingsMainContainer: HTMLElement | null) => {
+  if (!listingsMainContainer) return;
+  injectButtonsForContent(listingsMainContainer);
+  getSumActiveOrders(listingsMainContainer);
+  getSumBuyOrders(listingsMainContainer);
 };
 
 export { appendChangesToListings };
